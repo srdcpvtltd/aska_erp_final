894d279c4c2525bbf390e6ec5af4dfb9aedc343d
srdcpvtltd <debendraster@gmail.com>
2024-09-23 21:16:12 +0530

    Update footer.blade.php

--- resources/views/admin/section/footer.blade.php
+++ resources/views/admin/section/footer.blade.php
@@ -2,8 +2,7 @@
 
 <!-- partial:partials/_footer.html -->
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-    <p class="text-muted mb-1 mb-md-0">Copyright © <?php echo date("Y"); ?> <a href="https://cninfotech.com/" target="_blank">Cyclone Nepal</a>.</p>
-{{--    <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i> Bizberg Themes</p>--}}
+    <p class="text-muted mb-1 mb-md-0">Copyright © <?php echo date("Y"); ?> <a href="https://askasugar.in" target="_blank">ASKA Sugar</a>.</p>
 </footer>
 <!-- partial -->
 
