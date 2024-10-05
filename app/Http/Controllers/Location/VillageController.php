2ab830b342679b39a66b36c289e5bf234e7f11c9
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-10-05 17:53:37 +0530

    05/10/2024

--- app/Http/Controllers/Location/VillageController.php
+++ app/Http/Controllers/Location/VillageController.php
@@ -123,10 +123,8 @@ class VillageController extends Controller
             $zones->prepend('Select Zone', '');
             $centers = Center::all()->pluck('name', 'id');
             $centers->prepend('Select Center', '');
-            $blk = GramPanchyat::find($village->gram_panchyat_id);
-            $dstct = Block::find($blk->block_id);
 
-            return view('admin.location.village.edit', compact('village', 'districts', 'blocks', 'grampanchyats', 'zones', 'centers','blk', 'dstct'));
+            return view('admin.location.village.edit', compact('village', 'districts', 'blocks', 'grampanchyats', 'zones', 'centers'));
         } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
